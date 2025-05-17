78c37941307b4c99c6c768c66a7b5589a7e18bdd
Marcdeath <aurele2k06@gmail.com>
2025-05-17 14:29:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,3 +24,8 @@ def remove_bg():
 
                     if _name_ == '_main_':
                                         app.run(debug=True)
+                        import os
+
+                            if __name__ == "__main__":
+                         port = int(os.environ.get("PORT", 5000))  # Render passe le port via une variable d’environnement
+                         app.run(host="0.0.0.0", port=port)
